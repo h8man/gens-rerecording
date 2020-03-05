09ae80a1d35acea02027448653ec79b1317d834d
h8man <vmazurenko@bk.ru>
2020-03-05 23:52:26 +0200

    Serialize PlaneA and PlaneB in sync.

--- Gens-rr/Output/LuaSamples/vram_scrambler.lua
+++ Gens-rr/Output/LuaSamples/vram_scrambler.lua
@@ -1,6 +1,6 @@
 -- VRAM scramber's goal is to coolect Plane information,
 -- and serialize it with apropriate VScrol and HSroll shifts
--- essentially to serialize lvl tiles posotion
+-- essentially to serialize lvl tiles position
 
 function writebytes(f,x)
     local b2=string.char(x%256) x=(x-x%256)/256;
@@ -9,6 +9,7 @@ function writebytes(f,x)
 	f:write(b2);
 end
 
+--unsigned short to signed short (16 bits)
 function us2si(ss)
   if(AND(ss,0x8000) == 0x8000) then --negative
     ss = ss-0xFFFF;
@@ -16,6 +17,7 @@ function us2si(ss)
   return ss;
 end
 
+--display debug info and screen scroll coorinates
 function display()
   local basex = 200;
   local basey = 200;
@@ -46,32 +48,46 @@ function display()
   end
   
 end
+
+function writeAll(tiles, Nx, Ny, file_stream)
+  for j=1, Nx do
+   for i=1, Ny do
+    writebytes(file_stream,tiles[i][j]);
+   end
+  end
+end
+
 -- initialize
 print("VRAM Scramble - START")
 print(vdp);
 
-N=300; --1000 to the left and 1000 tj the right 
+N=500; -- -N to the left and N tj the right 
 Path = "D:\\Workspace\\"
 
-PlaneB = 0xE000;
-PlaneA = 0xC000;
 
-PLANE = PlaneA;
+PlaneB = 0xE000; --specific to game renderer mode
+PlaneA = 0xC000; --specific to game renderer mode
 
-hNTSC40 = 40;
-vNTSC40 = 28;
+--PLANE = PlaneA;
 
-hPlane = 64;
-vPlane = 32;
+hNTSC40 = 40; --specific to game renderer mode
+vNTSC40 = 28; --specific to game renderer mode
 
-wBaseX = 0x11;
-wBaseY = 0x1F;
+hPlane = 64; --specific to game renderer mode
+vPlane = 32; --specific to game renderer mode
 
-mt = {}
+mtA = {}
 for i=1,N do
-  mt[i] = {}     -- create a new row
+  mtA[i] = {}     -- create a new row
   for j=1,N do
-    mt[i][j] = 0x4fb
+    mtA[i][j] = 0x4fb --empty value
+  end
+end
+mtB = {}
+for i=1,N do
+  mtB[i] = {}     -- create a new row
+  for j=1,N do
+    mtB[i][j] = 0x4fb --empty value
   end
 end
   
@@ -79,17 +95,19 @@ gens.registerafter( function()
   -- put any code you want to run after each frame here
   -- (such as getting the last frame's input or reading from memory)
 
-  uHS = vdp.readwordvram(0x0000);
-  uVS = vdp.readwordvsram(0x0000);
+  uHS = vdp.readwordvram(0x0000); --usigned horizontal scroll value
+  uVS = vdp.readwordvsram(0x0000); --usigned vertical scroll value
 
   iHS = -us2si(uHS);
   iVS = us2si(uVS);
   
+  --pixels to tiles 8x8
   HS8 = math.floor(iHS/8) % hPlane;
   VS8 = math.floor(iVS/8) % vPlane;
   
   display();
   
+  --tile global coordinates in grid
   tile_x = math.floor(iHS/8)+math.floor(N/2);
   tile_y = math.floor(iVS/8)+math.floor(N/2);
   
@@ -101,9 +119,12 @@ gens.registerafter( function()
     for j=0, (vNTSC40-1), 1 do
 	  h_offset = (i+HS8)%hPlane;
 	  v_offset = (j+VS8)%vPlane;
-
-	  tile_index = vdp.readwordvram(PLANE+2*h_offset+2*v_offset*hPlane);
-	  mt[tile_x+i][tile_y+j]=tile_index;
+	  --un-flatern array
+	  tile_index = vdp.readwordvram(PlaneA+2*h_offset+2*v_offset*hPlane);
+	  mtA[tile_x+i][tile_y+j]=tile_index;
+	  
+	  tile_index = vdp.readwordvram(PlaneB+2*h_offset+2*v_offset*hPlane);
+	  mtB[tile_x+i][tile_y+j]=tile_index;
 	end
   end  
   
@@ -111,11 +132,10 @@ end)
 
 gens.registerexit( function()
   -- cleanup code (if any) goes here
-  local f = io.open(Path .. "tmp.bytes","wb");
-  for j=1, N do
-   for i=1, N do
-    writebytes(f,mt[i][j]);
-   end
-  end  
-  f:close();
+  local f1 = io.open(Path .. "PlaneA.bytes","wb");
+  writeAll(mtA, N, N, f1);
+  f1:close();
+  local f2 = io.open(Path .. "PlaneB.bytes","wb");
+  writeAll(mtB, N, N, f2);
+  f2:close();
 end) 
